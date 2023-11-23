79f7775e732e98fd3976e8f3ce8eba9bc40f9058
Hà Trần <hiimbias@gmail.com>
2023-11-23 15:16:41 +0700

    Create base file

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
+app = Flask(__name__)
+app.secret_key = '21112003'
+
+@app.route('/')
+def index():
+    return render_template('index1.html',
+                           username_error="",
+                           password_error="",
+                           registration_success='')
+    
+@app.route('/images/<filename>')
+
+def get_image(filename):
+    return send_from_directory('image', filename)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
